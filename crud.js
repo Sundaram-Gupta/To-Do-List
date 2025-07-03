3c4d8fee5c268f75c755e28221ab455161cbfc0b
Sundaram Gupta <160701494+Sundaram-Gupta@users.noreply.github.com>
2025-07-03 18:51:53 +0530

    Update crud.js
    changed the code for working of lists after saved into browser

--- crud.js
+++ crud.js
@@ -23,16 +23,8 @@ function addTask(){
         span.innerHTML = "\u00d7";
         console.log(tasks);
         li.appendChild(span);
-        
-        li.addEventListener("click",function(e){
-            if(e.target.tagName == "LI"){
-                e.target.classList.toggle("completed");
-            }
-            else if(e.target.tagName == "SPAN"){
-                e.target.parentElement.remove();
-            }
-            saveTask();
-        })  
+          
+        saveTask();
     }
     else{
         alert("You must write something!")
@@ -41,6 +33,15 @@ function addTask(){
     taskInp.focus();
     saveTask();
 }
+
+tasks.addEventListener("click",function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("completed");
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+    }
+})
 
 
 function saveTask(){
